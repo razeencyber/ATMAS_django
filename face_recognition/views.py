597ebf8a71798e39b681249fc2813e0fa49d508f
Uttarayan Mondal <uttarayan21@gmail.com>
2020-09-16 00:08:47 +0530

    Removed Images
    Did stuff

--- face_recognition/views.py
+++ face_recognition/views.py
@@ -16,7 +16,9 @@ def warn_face(request):
 
 def detectFace(request):
     face_detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor(str(BASE_DIR) + "/face_recognition/research/shape_predictor_68_face_landmarks.dat") #
+    predictor = dlib.shape_predictor(
+        str(BASE_DIR) +
+        "/face_recognition/research/shape_predictor_68_face_landmarks.dat")  #
 
     cap = cv2.VideoCapture(0)
 
@@ -24,27 +26,30 @@ def detectFace(request):
     recognizer.read(
         str(BASE_DIR) +
         "/face_recognition/research/trainer.yml")  #Learning the trained data
+
     def mid_point(p1, p2):
-        return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+        return int((p1.x + p2.x) / 2), int((p1.y + p2.y) / 2)
 
     def get_blink_ratio(eye_points, facial_landmarks):
-        left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
-        right_point = (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
-        mid_top = mid_point(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
-        mid_bottom = mid_point(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
-        
-        hor_dist = cv2.line(img, left_point, right_point, (255,0,0), 2)
-        ver_dist = cv2.line(img, mid_top, mid_bottom, (255,0,0), 2)
-        
-        hor_dist_length = hypot((left_point[0]-right_point[0]),(left_point[1]-right_point[1]))
-        ver_dist_length = hypot((mid_top[0] - mid_bottom[0]),(mid_top[1] - mid_bottom[1]))
+        left_point = (facial_landmarks.part(eye_points[0]).x,
+                      facial_landmarks.part(eye_points[0]).y)
+        right_point = (facial_landmarks.part(eye_points[3]).x,
+                       facial_landmarks.part(eye_points[3]).y)
+        mid_top = mid_point(facial_landmarks.part(eye_points[1]),
+                            facial_landmarks.part(eye_points[2]))
+        mid_bottom = mid_point(facial_landmarks.part(eye_points[5]),
+                               facial_landmarks.part(eye_points[4]))
 
-        ratio = hor_dist_length/ver_dist_length
+        hor_dist = cv2.line(img, left_point, right_point, (255, 0, 0), 2)
+        ver_dist = cv2.line(img, mid_top, mid_bottom, (255, 0, 0), 2)
+
+        hor_dist_length = hypot((left_point[0] - right_point[0]),
+                                (left_point[1] - right_point[1]))
+        ver_dist_length = hypot((mid_top[0] - mid_bottom[0]),
+                                (mid_top[1] - mid_bottom[1]))
+
+        ratio = hor_dist_length / ver_dist_length
         return ratio
-
-
-
-
 
     getId = 0
     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -61,22 +66,28 @@ def detectFace(request):
         faces = face_detector(gray)
         for face in faces:
             x, y = face.left(), face.top()
-            x1,y1 = face.right(), face.bottom()
+            x1, y1 = face.right(), face.bottom()
             cv2.rectangle(img, (x, y), (x1, y1), (0, 255, 0), 2)
 
             #Implementing Blink Detection to prevent spoofing
             landmarks = predictor(gray, face)
-            left_eye_ratio = get_blink_ratio([36, 37, 38, 39, 40, 41], landmarks)
-            right_eye_ratio = get_blink_ratio([42, 43, 44, 45, 46, 47], landmarks)
+            left_eye_ratio = get_blink_ratio([36, 37, 38, 39, 40, 41],
+                                             landmarks)
+            right_eye_ratio = get_blink_ratio([42, 43, 44, 45, 46, 47],
+                                              landmarks)
 
-            avg_blink_ratio = (left_eye_ratio + right_eye_ratio)/2
+            avg_blink_ratio = (left_eye_ratio + right_eye_ratio) / 2
 
             #Implementing Real time facial recognition
-            getId, conf = recognizer.predict(gray[y:y1, x:x1])
-            print(f"getId {getId} card_number {card_number} conf {conf}")
-            
-            
-            if conf < 47 and getId == int(card_number):
+            try:
+                getId, conf = recognizer.predict(gray[y:y1, x:x1])
+            except:
+                return redirect('/warn_face')
+            print(
+                f"getId {getId} card_number {card_number} conf {conf} blinking_ratio {avg_blink_ratio}"
+            )
+
+            if conf < 65 and getId == int(card_number):
                 if avg_blink_ratio > 6.0:
                     userId = getId
                     cv2.putText(img, "Detected", (x, y1), font, 2, (0, 255, 0),
@@ -84,14 +95,13 @@ def detectFace(request):
                 else:
                     get_frame += 1
                     cv2.rectangle(img, (x, y), (x1, y1), (0, 0, 255), 2)
-                    cv2.putText(img, "Fake", (x, y1), font, 2, (0, 0, 255),
-                            2)
-                    cv2.putText(img, "Please Blink",(20,150), font, 2, (255,0,0),3)
-                
+                    cv2.putText(img, "Fake", (x, y1), font, 2, (0, 0, 255), 2)
+                    cv2.putText(img, "Please Blink", (20, 150), font, 2,
+                                (255, 0, 0), 3)
+
             else:
                 get_frame += 1
-                cv2.putText(img, "Unknown", (x, y1), font, 2, (0, 0, 255),
-                            2)
+                cv2.putText(img, "Unknown", (x, y1), font, 2, (0, 0, 255), 2)
         cv2.imshow("Face", img)
         if (cv2.waitKey(1) == ord('q')):
             break
